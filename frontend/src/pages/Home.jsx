057964b2b6e71199468121061d56a1423366a15e
The Exorcist <whitebeard086@gmail.com>
2022-07-01 14:43:14 +0100

    fetch workouts from db

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react";
+import { Workout } from "../components";
+
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
-export default Home
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const data = await response.json();
+
+      if (response.ok) {
+        setWorkouts(data);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
+  return <div>
+    <div className="workouts">
+        {workouts && workouts.map((workout) => (
+            <Workout key={workout._id} workout={workout} />
+        ))}
+    </div>
+  </div>;
+};
+export default Home;
