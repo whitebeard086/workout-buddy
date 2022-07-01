7e93f74c819d34efc19b391b58c780745bd7f16e
The Exorcist <whitebeard086@gmail.com>
2022-07-01 05:08:44 +0100

    api routes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
 require("dotenv").config();
 
 const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 // initialize express app
 const app = express();
 
 // middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to the app!" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () => {
