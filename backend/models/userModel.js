79ab1eb142d318b09751f309574c6caa5a1c6bce
The Exorcist <whitebeard086@gmail.com>
2022-08-03 08:18:00 +0100

    Signing up user and hashing password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -16,5 +17,24 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+    const userExists = await this.findOne({ email });
+
+    if (userExists) {
+        throw new Error("Email already in use!")
+    }
+
+    // generate salt
+    const salt = await bcrypt.genSalt(10);
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create user
+    const user = await this.create({ email, password: hashedPassword });
+
+    return user;
+}
 
 module.exports = mongoose.model("User", userSchema);
