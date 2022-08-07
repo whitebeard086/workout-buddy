d988af278998aadcb86dbaddb5438c5eb06b6117
The Exorcist <whitebeard086@gmail.com>
2022-08-07 18:11:06 +0100

    useSignup hook

---
+++ frontend/src/hooks/useSignup.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+
+  const { dispatch } = useAuthContext();
+
+  const signup = async (email, password) => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch("/api/user/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(data.error);
+    }
+    if (response.ok) {
+      // save the usr to local storage
+      localStorage.setItem("user", JSON.stringify(data));
+
+      // update the auth context
+      dispatch({ type: "LOGIN", payload: data });
+
+      setIsLoading(false);
+    }
+  };
+
+  return { signup, error, isLoading };
+};
