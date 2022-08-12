4c69ad312519faee5e053bbe45cb1f04aabb7862
The Exorcist <whitebeard086@gmail.com>
2022-08-12 20:02:30 +0100

    Protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import { useAuthContext } from "./hooks/useAuthContext";
 import { Navbar } from "./components";
 import { Home, Login, Signup } from "./pages";
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <Router>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
       </Router>
